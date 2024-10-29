81001c2dde5c9cf608e694955e5157b497c7fa0c
FannIrfan <irfanfadhlurrohman05@gmail.com>
2024-10-29 16:09:24 +0700

    pert4

--- pertemuan 4/app/views/userView.php
+++ pertemuan 4/app/views/userView.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Vicky Maulana S-MVC</title>
+    <title>Akhmad Irfan S-MVC</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/128/15466/15466052.png" type="image/x-icon">
     <style>
